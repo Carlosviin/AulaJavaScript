db8d424420cd0441e0add01cb8a783eccc566af3
Carlosviin <carlosvin26@gmail.com>
2021-09-13 17:00:32 -0300

    foi criado o arquivo atividade.js

---
+++ Atividade.js
@@ -0,0 +1,22 @@
+
+
+
+const Peças = ['la', 'bla bla', 'bla bla bla', 'le', 'ble ble','ble ble ble','li','bli bli', 'bli bli bli', 'lo']
+    if (Peças.length <= 10){
+        console.log("Liberado para pesagem")
+    } else {
+        console.log('Capacidade insuficiente')
+    }
+console.log('pesagem')
+
+
+
+console.log('quantidade de caracteres')
+ for (let quant = 0; quant < Peças.length; quant++){
+     const atual = Peças [quant]
+     if(atual.length < 3){
+         console.log(atual + ' :Coloque um nome superior a 3 caracteres')
+     } else {
+         console.log(atual + ' :Esse nome foi liberado para cadastro')
+     }
+ }
